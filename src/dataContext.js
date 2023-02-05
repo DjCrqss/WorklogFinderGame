050a3bf8e9bd890728430db2f1024647e0edb8c5
Declan Cross <dj3dxx@gmail.com>
2023-02-05 20:04:57 +1300

    Fix for full links with code autofill plus poor man's encryption built in

--- src/dataContext.js
+++ src/dataContext.js
@@ -8,7 +8,6 @@ export const DataContextProvider = (props) => {
     const objectiveAmount = 4; // up to 8 looks best
     const screenCoveringAmount = objectiveAmount + 2;
     const [data, setData] = useState(loadSavedData());
-    qrDecryptAndSave(window.location.search);
     const scale = 1.4;
 
 
@@ -19,44 +18,14 @@ export const DataContextProvider = (props) => {
     const [height, setHeight] = useState(window.innerHeight/screenCoveringAmount * scale);
 
 
-    function qrDecryptAndSave(link){
-        let newData = data;
+   
 
-        // allow both URL input and direct code input
-        let code;
-        try {
-            const url = new URL(link);
-            const args =  new URLSearchParams(url.search);
-            code = args.get("code");
-        } catch (error) {}
-        let result = code || link;
-        
-        if(result === "reset_all"){
-            newData.forEach((value) => {value.isCollected = false});
-        }
-        else if(!isNaN(result) && +result > 0 && +result <= data.length){
-            newData[+result - 1].isCollected = true;
-        } else {
-            return false;
-        }
-        // get decoded text and check if it is a number to set to true
-        setData(newData);
-
-        // save data
-        console.log("Data saved with new entry from", result);
-        localStorage.setItem("data", JSON.stringify(data));
-
-        confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.8 },
-            colors : ['#fcba03', '#fca103', '#ebf263', '#ffd996', '#fff5c2']
-          });
-
-        return true;
-    }
-
-
+    // Startup functions
+    useEffect(() => {
+        qrDecryptAndSave(window.location);
+       window.addEventListener('resize', handleResize)
+       return () => window.removeEventListener('resize', handleResize)
+    }, []);
 
     // Checks if this is the first time the user opens the app
     function checkFirstLoad(){
@@ -98,15 +67,53 @@ export const DataContextProvider = (props) => {
         return cards;
     }
 
+    function qrDecryptAndSave(link){
+        let newData = data;
+
+        // allow both URL input and direct code input
+        let code;
+        try {
+            const url = new URL(link);
+            const args =  new URLSearchParams(url.search);
+            code = args.get("code");
+        } catch (error) {}
+        let result = code || link;
+        
+        if(result === "reset_all"){
+            newData.forEach((value) => {value.isCollected = false});
+        }
+        else if(!isNaN(result)){
+            let num = +result;
+            num /= ssc;
+            if(num <= 0 || num > data.length || !Number.isInteger(num)){
+                return false;
+            }
+            if(newData[num - 1].isCollected === false){
+                confetti({
+                    particleCount: 100,
+                    spread: 70,
+                    origin: { y: 0.8 },
+                    colors : ['#fcba03', '#fca103', '#ebf263', '#ffd996', '#fff5c2']
+                });
+            }
+            newData[num - 1].isCollected = true;
+        } else {
+            return false;
+        }
+        // get decoded text and check if it is a number to set to true
+        setData(newData);
+
+        // save data
+        console.log("Data saved with new entry from", result);
+        localStorage.setItem("data", JSON.stringify(data));
+
+        return true;
+    }
 
     // Handles resize and variable changes
     function handleResize(){
         setHeight(window.innerHeight/screenCoveringAmount * scale);
     };
-    useEffect(() => {
-       window.addEventListener('resize', handleResize)
-       return () => window.removeEventListener('resize', handleResize)
-    }, []);
     useEffect(()=>{
         // setBlocks(createBlocks());
         handleResize();
@@ -128,6 +135,6 @@ export const DataContextProvider = (props) => {
         qrDecryptAndSave
     }
 
+    const ssc = 423;
     return <DataContext.Provider value={dataContextStore}>{props.children}</DataContext.Provider>
-
 }
